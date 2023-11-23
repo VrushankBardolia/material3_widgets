a10a1f52a3d4107eb64fe86614783ab9e2e53c8f
VrushankBardolia <vrushank1793@gmail.com>
2023-11-23 13:39:29 +0530

    Committed

--- lib/widgets/tabbar.dart
+++ lib/widgets/tabbar.dart
@@ -29,7 +29,45 @@ class TabbarDemo extends StatelessWidget {
           ),
           body: TabBarView(
             children: [
-
+              Card(
+                shadowColor: Colors.transparent,
+                margin: const EdgeInsets.all(8.0),
+                child: SizedBox.expand(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.directions_car_filled_rounded),
+                      Text('Car',style: Theme.of(context).textTheme.titleMedium,),
+                    ],
+                  )
+                ),
+              ),
+              Card(
+                shadowColor: Colors.transparent,
+                margin: const EdgeInsets.all(8.0),
+                child: SizedBox.expand(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.train_outlined),
+                      Text('Train',style: Theme.of(context).textTheme.titleMedium,),
+                    ],
+                  )
+                ),
+              ),
+              Card(
+                shadowColor: Colors.transparent,
+                margin: const EdgeInsets.all(8.0),
+                child: SizedBox.expand(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.flight_rounded),
+                      Text('Flight',style: Theme.of(context).textTheme.titleMedium,),
+                    ],
+                  )
+                ),
+              ),
             ],
           ),
         )
