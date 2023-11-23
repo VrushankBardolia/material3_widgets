d5b5c79b360d3f1eafe4035014b8669ac1099775
VrushankBardolia <vrushank1793@gmail.com>
2023-11-23 18:37:32 +0530

    Navigation Rail options added

--- lib/widgets/navigationRail.dart
+++ lib/widgets/navigationRail.dart
@@ -9,6 +9,10 @@ class NavigationRailDemo extends StatefulWidget {
 
 class _NavigationRailDemoState extends State<NavigationRailDemo> {
   int _selectedIndex = 0;
+  NavigationRailLabelType labelType = NavigationRailLabelType.all;
+  bool showLeading = false;
+  bool showTrailing = false;
+  double groupAlignment = -1.0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,52 +20,182 @@ class _NavigationRailDemoState extends State<NavigationRailDemo> {
       appBar: AppBar(
         title: const Text('Navigation Rail'),
       ),
-      body: Row(
-        children: [
+      body: SafeArea(
+        child: Row(
+          children: [
 
-          // Main Component (Navigation Rail)
-          NavigationRail(
-            selectedIndex: _selectedIndex,
-            labelType: NavigationRailLabelType.selected,
-            onDestinationSelected: (int index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            leading: FloatingActionButton(
-              onPressed: (){},
-              elevation: 0,
-              child: const Icon(Icons.add),
+            // Main Component (Navigation Rail)
+            NavigationRail(
+              selectedIndex: _selectedIndex,
+              labelType: labelType,
+              groupAlignment: groupAlignment,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              leading: showLeading ? FloatingActionButton(
+                onPressed: (){},
+                elevation: 0,
+                child: const Icon(Icons.add),
+              )  : null,
+              trailing: showTrailing ? const Icon(Icons.more_horiz) : null,
+              destinations: const [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home_outlined),
+                    selectedIcon: Icon(Icons.home),
+                    label: Text('Home')
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite_border_outlined),
+                    selectedIcon: Icon(Icons.favorite),
+                    label: Text('Favourite')
+                ),
+                NavigationRailDestination(
+                    icon:Icon(Icons.star_border),
+                    selectedIcon: Icon(Icons.star),
+                    label: Text('Notification')
+                ),
+              ],
             ),
-            trailing: const Icon(Icons.more_horiz),
-            destinations: const [
-              NavigationRailDestination(
-                  icon: Icon(Icons.home_outlined),
-                  selectedIcon: Icon(Icons.home),
-                  label: Text('Home')
-              ),
-              NavigationRailDestination(
-                  icon: Icon(Icons.favorite_border_outlined),
-                  selectedIcon: Icon(Icons.favorite),
-                  label: Text('Favourite')
-              ),
-              NavigationRailDestination(
-                  icon:Icon(Icons.star_border),
-                  selectedIcon: Icon(Icons.star),
-                  label: Text('Notification')
-              ),
-            ],
-          ),
 
-          // Vertical Divider
-          const VerticalDivider(),
+            // Vertical Divider
+            const VerticalDivider(),
 
-          // Settings
-          Column(
-
-          )
-        ],
-      ),
+            // Settings
+            Column(
+              children: [
+                Text('Label Type',style: Theme.of(context).textTheme.titleMedium),
+                Text(labelType.name),
+                SizedBox(
+                  width: 250,
+                  child: RadioListTile(
+                      title: const Text('None'),
+                      value: NavigationRailLabelType.none,
+                      groupValue: labelType,
+                      onChanged: (value){
+                        setState(() {
+                          labelType = value!;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: RadioListTile(
+                      title: const Text('Selected'),
+                      value: NavigationRailLabelType.selected,
+                      groupValue: labelType,
+                      onChanged: (value){
+                        setState(() {
+                          labelType = value!;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: RadioListTile(
+                      title: const Text('All'),
+                      value: NavigationRailLabelType.all,
+                      groupValue: labelType,
+                      onChanged: (value){
+                        setState(() {
+                          labelType = value!;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(width: 250, child: Divider(thickness: 1)),
+                Text('Group Alignment',style: Theme.of(context).textTheme.titleMedium),
+                Text('$groupAlignment'),
+                SizedBox(
+                  width: 250,
+                  child: RadioListTile(
+                    title: Text('Top'),
+                      value: -1.0,
+                      groupValue: groupAlignment,
+                      onChanged: (value){
+                        setState(() {
+                          groupAlignment = value!;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: RadioListTile(
+                    title: const Text('Middle'),
+                      value: 0.0,
+                      groupValue: groupAlignment,
+                      onChanged: (value){
+                        setState(() {
+                          groupAlignment = value!;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: RadioListTile(
+                    title: const Text('Bottom'),
+                      value: 1.0,
+                      groupValue: groupAlignment,
+                      onChanged: (value){
+                        setState(() {
+                          groupAlignment = value!;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(width: 250, child: Divider(thickness: 1)),
+                SizedBox(
+                  width: 250,
+                  child: SwitchListTile(
+                    title: Text('Show Leading'),
+                      value: showLeading,
+                      onChanged: (bool value){
+                        setState(() {
+                          showLeading = value;
+                        });
+                      }
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: SwitchListTile(
+                    title: Text('Show Trailing'),
+                      value: showTrailing,
+                      onChanged: (bool value){
+                        setState(() {
+                          showTrailing = value;
+                        });
+                      }
+                  ),
+                )
+                // FilledButton.tonal(
+                //   onPressed: () {
+                //     setState(() {
+                //       showLeading = !showLeading;
+                //     });
+                //   },
+                //   child:
+                //   Text(showLeading ? 'Hide Leading' : 'Show Leading'),
+                // ),
+                // FilledButton.tonal(
+                //   onPressed: () {
+                //     setState(() {
+                //       showTrailing = !showTrailing;
+                //     });
+                //   },
+                //   child:
+                //   Text(showTrailing ? 'Hide Leading' : 'Show Leading'),
+                // )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
